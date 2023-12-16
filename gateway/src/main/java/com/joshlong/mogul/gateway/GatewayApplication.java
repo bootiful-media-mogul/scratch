fc50859e124680848aa4d155f6f586346399d9a3
Josh Long <josh@joshlong.com>
2023-12-15 23:49:30 -0800

    polish

--- gateway/src/main/java/com/joshlong/mogul/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/joshlong/mogul/gateway/GatewayApplication.java
@@ -1,25 +1,14 @@
 package com.joshlong.mogul.gateway;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.util.Assert;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.net.URI;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GatewayProperties.class)
