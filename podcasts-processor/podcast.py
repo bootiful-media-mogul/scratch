abb1f13f56ac4de434258db52ea8857788702c5b
Josh Long <josh@joshlong.com>
2024-03-09 16:49:10 -0800

    up

--- podcasts-processor/podcast.py
+++ podcasts-processor/podcast.py
@@ -10,14 +10,34 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
 
-def create_podcast(
-        asset_intro_file: str,
-        asset_music_segue_file: str,
-        asset_closing_file: str,
-        episode_intro_file: str,
-        episode_interview_file: str,
-        output_directory: str
-):
+# the idea is that there are any arbitrary number of audio segments to be combined together and or crossfaded.
+# the client will send us files and crossfade information that we need to respect
+
+class Segment(object):
+    def __init__(self, audio_file: str, ext: str, crossfade_time: int = 100):
+        self.audio_file = audio_file
+        self.extension = ext
+        self.crossfade_time = crossfade_time
+
+
+def create_podcast(audio_files: typing.List[Segment], output_file_path: str, output_extension='mp3') -> str:
+    """ creates podcast a then returns the file path of the resulting audio file """
+    assert len(audio_files) > 0, 'there should be at least one segment for this clip'
+    assert audio_files is not None, 'there should be at least one segment for this'
+    assert output_file_path is not None, 'you should provide an output file path'
+    path_dirname = os.path.dirname(output_file_path)
+    assert os.path.exists(path_dirname) or os.makedirs(
+        path_dirname), f'the directory to which you want to write, {path_dirname}, does not exist.'
+    audio_segments = [(AudioSegment.from_file(a.audio_file, format=a.extension), a.crossfade_time) for a in audio_files]
+    out =   audio_segments[0][0]
+    for segment, crossfade_time in audio_segments[1:]:
+        out = out.append(segment, crossfade=crossfade_time)
+    out.export(output_file_path, format=output_extension, bitrate="256k")
+    return output_file_path
+
+
+def old_create_podcast(asset_intro_file: str, asset_music_segue_file: str, asset_closing_file: str,
+                       episode_intro_file: str, episode_interview_file: str, output_directory: str):
     assert os.path.exists(output_directory) or reset_and_recreate_directory(
         output_directory
     ), ("the directory %s does not exist " "and couldn't be created" % output_directory)
@@ -53,11 +73,7 @@ def create_podcast(
     return return_value
 
 
-if __name__ == "__main__":
-    import os
-    import os.path
-
-
+def old_main():
     def valid_path_env_var(k: str) -> str:
         assert k is not None and k in os.environ, (
                 "there is no environment variable called %s" % k
@@ -67,7 +83,6 @@ if __name__ == "__main__":
                 "the directory pointed to by %s does not exist" % k
         )
         return value
-
 
     assets_dir: str = valid_path_env_var("PODCAST_ASSETS_DIR")
     output_dir: str = valid_path_env_var("PODCAST_OUTPUT_DIR")
